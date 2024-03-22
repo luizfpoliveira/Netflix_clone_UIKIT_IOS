938893da44370c153e8795c2e4f810ec90e213d4
luiz Oliveira <luizfpeoliveira@gmail.com>
2024-03-22 12:26:54 -0300

    Initial Commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Luiz Oliveira on 22/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
