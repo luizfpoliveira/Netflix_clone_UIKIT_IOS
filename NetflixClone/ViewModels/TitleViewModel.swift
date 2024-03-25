f434a27473a90c095c7f7b2e01cbfb6037fdb666
luiz Oliveira <luizfpeoliveira@gmail.com>
2024-03-25 15:50:00 -0300

    Finished Upcoming Movies Tab

---
+++ NetflixClone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NetflixClone
+//
+//  Created by Luiz Oliveira on 25/03/24.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
---
+++ NetflixClone/Views/TitleTableViewCell.swift
@@ -0,0 +1,80 @@
+//
+//  TitleTableViewCell.swift
+//  NetflixClone
+//
+//  Created by Luiz Oliveira on 25/03/24.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+    
+    private let playTitleButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(systemName: "play.circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30))
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+    
+        return button
+    }()
+    
+    private let titlePosterImageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFill
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.clipsToBounds = true
+        return image
+    }()  
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    static let identifier = "TitleTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(titlePosterImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(playTitleButton)
+        configureContraints()
+    }
+    
+    private func configureContraints(){
+        NSLayoutConstraint.activate([
+            titlePosterImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titlePosterImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            titlePosterImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            titlePosterImageView.widthAnchor.constraint(equalToConstant: 100),
+            
+            titleLabel.leadingAnchor.constraint(equalTo: titlePosterImageView.trailingAnchor, constant: 20),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            titleLabel.widthAnchor.constraint(equalToConstant: 200),
+            
+            playTitleButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            playTitleButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            
+        ])
+    }
+    
+    public func configure(with model: TitleViewModel) {
+        
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500/\(model.posterURL)") else {
+            return
+        }
+        
+        titlePosterImageView.sd_setImage(with: url, completed: nil)
+        titleLabel.text = model.titleName
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
